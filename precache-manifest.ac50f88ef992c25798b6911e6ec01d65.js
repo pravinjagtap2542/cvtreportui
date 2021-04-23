9ca1da2c4d12ec233425964d46d7e4edd5946c2d
Jagtap, Pravin (Pravin) **CTR** <pjagtap@avaya.com>
2021-04-23 12:41:48 +0530

    Updates

---
+++ precache-manifest.ac50f88ef992c25798b6911e6ec01d65.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = [
+  {
+    "revision": "28d0e74afa0af51860c8",
+    "url": "/cvtreportui/static/css/main.a10ddcc7.chunk.css"
+  },
+  {
+    "revision": "28d0e74afa0af51860c8",
+    "url": "/cvtreportui/static/js/main.28d0e74a.chunk.js"
+  },
+  {
+    "revision": "085ff856332886e4150d",
+    "url": "/cvtreportui/static/js/1.085ff856.chunk.js"
+  },
+  {
+    "revision": "538a544f3d779a51e7b1",
+    "url": "/cvtreportui/static/js/runtime~main.538a544f.js"
+  },
+  {
+    "revision": "6c426583a257819912278cedfdd9badf",
+    "url": "/cvtreportui/static/media/item1.6c426583.jpg"
+  },
+  {
+    "revision": "8779dd15782203466bfb67cac99ef2ec",
+    "url": "/cvtreportui/static/media/item2.8779dd15.jpg"
+  },
+  {
+    "revision": "9bd2546f523e4a6e2515068c65ca441c",
+    "url": "/cvtreportui/static/media/item3.9bd2546f.jpg"
+  },
+  {
+    "revision": "65bc33d70f7f4ca793c5acc24525de9f",
+    "url": "/cvtreportui/static/media/item4.65bc33d7.jpg"
+  },
+  {
+    "revision": "f5fd97b56137c3ed5df0f3f71482c105",
+    "url": "/cvtreportui/static/media/item6.f5fd97b5.jpg"
+  },
+  {
+    "revision": "c14343aefbe43e031aa4b1a6070b429b",
+    "url": "/cvtreportui/index.html"
+  }
+];
